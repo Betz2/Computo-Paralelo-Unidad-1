f4932a7dcf288552a4042698d82862c4ccac2590
Betz2 <itzxkovinebm1@outlook.com>
2025-08-21 01:40:23 +0000

    Creat codigo test

--- codes/philosophers/main.cpp
+++ codes/philosophers/main.cpp
@@ -1,1 +1,8 @@
 #include <iostream>
+
+int main()
+{
+    std::cout << "Stupid beaner" << std::endl;
+
+    return 0;
+}
---
+++ codes/test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 2;
+    int b = 3;
+    int c = 0;
+
+    c = a + b;
+
+    std::cout << a << " + " << b << " c" << std::endl;
+
+    return 0;
+}
